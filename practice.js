ab5db4086f7bbd2c9a2f020b3cb152ec0f30a6c2
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2026-01-16 09:59:53 +0500

    Started typescript :)

---
+++ practice.js
@@ -0,0 +1,14 @@
+var message = "Hello, TypeScript!";
+console.log(message);
+var username = "Nina";
+var age = 23;
+var country = "Ireland";
+console.log("Name is ".concat(username, " and age is ").concat(age, ", I live in ").concat(country));
+var numbers = [1, 2, 3, 4, 5];
+console.log("First score", numbers[0]);
+var user = {
+    name: "Nina",
+    email: "nina@gmail.com",
+    age: 23,
+};
+console.log(user);
