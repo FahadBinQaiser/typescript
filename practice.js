80d144f975ce6b2709a4b66834c217b216af6bf3
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2026-01-16 11:57:48 +0500

    Filtering out

--- practice.js
+++ practice.js
@@ -34,3 +34,12 @@ function calculateAverage(numbers) {
     return sum / numbers.length;
 }
 console.log(calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+function notTeen(users) {
+    return users.filter(function (user) { return user.age > 18; });
+}
+var users = [
+    { name: "Fahad", age: 17, id: 1 },
+    { name: "Nina", age: 20, id: 2 },
+    { name: "Steve", age: 19, id: 3 },
+];
+console.log(notTeen(users));
