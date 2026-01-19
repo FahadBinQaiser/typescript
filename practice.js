ac4132653c224f02f5016dff69d4f6e73ef17267
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2026-01-19 11:00:41 +0500

    2 functions done

--- practice.js
+++ practice.js
@@ -1,57 +1,22 @@
-function add(a, b) {
-    return a + b;
-}
-console.log(add(2, 4));
-function person(name, age) {
-    if (age) {
-        return "Hey my name is ".concat(name, " and age is ").concat(age);
-    }
-    else {
-        return "Hey my name is ".concat(name);
-    }
-}
-console.log(person("Fahad", 17));
-function multiply(x, y) {
-    if (y === void 0) { y = 2; }
-    return x * y;
-}
-console.log(multiply(5));
-console.log(multiply(5, 10));
-var divide = function (x, y) {
-    return x / y;
-};
-console.log(divide(10, 2));
-var logMessage = function (message) {
-    console.log(message);
-};
-logMessage("This function shows nothing");
-function calculateAverage(numbers) {
-    var sum = 0;
-    numbers.forEach(function (num) { return (sum += num); });
-    //   for (let num of numbers) {
-    //     sum += num;
-    //   }
-    return sum / numbers.length;
-}
-console.log(calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
-function findUserById(userList, id) {
-    return userList.find(function (userFinded) { return userFinded.id == id; });
-}
-var userList = [
-    { name: "Claudio", age: 17, id: 1 },
-    { name: "Nina", age: 20, id: 2 },
-    { name: "Steve", age: 19, id: 3 },
+"use strict";
+// function add(a: number, b: number): number {
+//   return a + b;
+// }
+// console.log(add(2, 4));
+Object.defineProperty(exports, "__esModule", { value: true });
+var console = require("node:console");
+var userFilledList = [
+    { name: "Claudio", age: 17, email: "nigga@gmail.com", id: 1 },
+    { name: "Nina", age: 20, email: "nina@gmail.com", id: 2 },
+    { name: "Steve", age: 19, email: "steve@gmail.com", id: 3 },
+    { name: "JohnWick", age: 36, email: "john@gmail.com", id: 4 },
+    { name: "Harry Pottah", age: 32, email: "pottah@gmail.com", id: 5 },
 ];
-console.log(findUserById(userList, 2));
-function isValidEmail(email) {
-    return email.includes("@");
+function getAdults(userList) {
+    return userList.filter(function (user) { return user.age > 18; });
 }
-console.log(isValidEmail("fahadbinqaiser3gmail.com"));
-function conversion(celsius) {
-    return (celsius * 9 / 5) + 32;
+console.log(getAdults(userFilledList));
+function findByEmail(user, email) {
+    return user.find(function (u) { return u.email === email; });
 }
-console.log(conversion(32));
-console.log(conversion(0)); // 32
-console.log(conversion(100)); // 212
-console.log(conversion(37)); // 98.6
-console.log(conversion(-40)); // -40
+console.log(findByEmail(userFilledList, "nina@gmail.com"));
